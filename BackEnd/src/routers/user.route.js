7dc3dd284645c4d1cde3b9e37fd1e043667e4ac1
ritesh6191 <158499208+ritesh6191@users.noreply.github.com>
2025-04-01 19:55:32 +0530

    done with user login

--- BackEnd/src/routers/user.route.js
+++ BackEnd/src/routers/user.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 
 const router = Router();
+
+router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
 
